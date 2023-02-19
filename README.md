956e920b4bb75c86cfe9250295a09479b52e0049
IceFlamesStudio <ice-flamesstudio@outlook.com>
2023-02-19 19:58:02 +0200

    addDocs

--- README.md
+++ README.md
@@ -1,1 +1,96 @@
-# Portal_Gaming_Hub_Python
+## Table of Contents
+
+1. [Portal Gaming Hub - Console Version](#portal-gaming-hub---console-version)
+2. [Tutorial](#tutorial)
+3. [How does it work](#how-does-it-work)
+4. [For the program to work correctly](#for-the-program-to-work-correctly)
+5. [Requirements](#requirements)
+6. [Configuration](#configuration)
+7. [Contributors](#contributors-✨)
+8. [Usage](#usage)
+9. [Support](#support)
+10. [License](#license)
+
+
+# Portal Gaming Hub - Console Version
+
+This code implements a number guessing mini-game, which can be run from the command line. The user navigates through menus to select a platform and then a game, after which they must guess a number within a randomly generated interval while receiving clues about the current interval. The game stores information such as the last time it was played and the current interval limits. 
+
+Additionally, the "main_menu" function displays a series of options and has a different color scheme based on ANSI escape characters if it is run on a Windows operating system with color support.
+
+# Tutorial
+
+[![Portal Gaming Hub in Console](https://img.youtube.com/vi/F-H_VAORFRk/0.jpg)](https://www.youtube.com/watch?v=F-H_VAORFRk)
+
+# How does it work:
+
+- The code imports several libraries: json, time, random, datetime, os, and platform.
+- It then checks the type of platform and enables color support on the command prompt if the platform is Windows.
+- The main_menu function clears the console and prints out some options.
+- The platforms_menu function clears the console and prints out a list of available platforms. It then prompts the user to select a platform and returns the available games for that platform.
+- The games_menu function clears the console and prints out a list of available games. It then prompts the user to select a game and starts a mini-game where the user has to guess a randomly generated number within a certain range.
+- The generate_number function generates a new random number each time it is called, within a certain range determined by the time since the last time the game was played.
+- The countdownMiniGame function is called to give a short countdown before displaying the next message.
+
+# For the program to work correctly:
+- Ensure that you have all the requirements.txt installed. ( pip install -r requirements.txt )
+- Make sure that you have selected the correct path in game_location, in platforms.json
+
+# Requirements
+
+Python 3.6 or later
+Required packages listed in requirements.txt
+To install the required packages, run the following command in your terminal:
+
+```sh
+pip install -r requirements.txt
+```
+
+# Configuration
+
+The script requires path in order to execute .exe files that will be mention in platforms.json, field "game_location".
+
+Example of platforms.json
+
+For example game League of Legends
+
+```json
+{
+    "Riot Games": {
+        "platform_name_suggestion": "Try playing a game outside instead of playing Steam games.",
+        "available_games": {
+            "League of Legends": {
+                "suggestion": "Why not go for a walk in the park instead of playing League of Legends?",
+                "game_location": "C:\\Riot Games\\League of Legends\\LeagueClient.exe",
+                "last_played": 1676827442.9226286
+            }
+        }
+    }
+}
+```
+
+# Contributors ✨
+
+Thanks go to these wonderful people:
+
+<table>
+  <tbody>
+    <tr>
+      <td align="center"><a href="https://axbecher.com"><img src="https://avatars.githubusercontent.com/u/72851811?v=4" width="100px;" alt="Alexandru Becher"/><br /><sub><b>Alexandru Becher</b></sub></a><br />
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+# Usage
+1. To run the script, use the following command in your terminal:
+```sh
+python main.py
+```
+2. Double click on main.py and console will pop up.
+
+# Support
+For any questions or support, please contact me via https://axbecher.com/contact/
+
+# License
+This project is licensed under the MIT License.
---
+++ main.py
@@ -0,0 +1,136 @@
+import json
+import time
+import random
+import datetime
+import os
+import platform
+
+if platform.system() == 'Windows':
+    os.system('color') # enable color support on Windows command prompt
+
+def main_menu():
+    os.system('cls') # clear console
+    print("\033[1;32m Select an option: \033[0m") 
+    print("\033[2;37m     exit - Exit the program \033[0m") 
+    print("\033[1;34m     platforms \033[0m\033[1;32m-\033[0m\033[1;34m Check available platforms\033[0m")
+    print("\n")
+
+def platforms_menu(platforms):
+    os.system('cls') # clear console
+    print("\033[1;32m Select a platform:\033[0m") # print in green
+    print("\033[2;37m     back to menu - return to main menu\033[0m") # print in red
+    for platform in platforms:
+        print("\033[1;34m     " + platform + "\033[0m") # print in blue
+    platform_choice = input("\n \033[7;32m Platform choice: \033[0m ")
+    if platform_choice == "back to menu":
+        return
+    if platform_choice in platforms:
+        games_menu(platforms[platform_choice]["available_games"])
+    else:
+        print("Invalid choice")
+
+def games_menu(games):
+    os.system('cls') # clear console
+    print("\033[1;32m Select a game:\033[0m") # print in green
+    print("\033[2;37m     back to menu - return to main menu\033[0m") # print in red
+    for game_name, game_data in games.items():
+        if "last_played" in game_data:
+            last_played = game_data["last_played"]
+            global last_played_old
+            last_played_old = game_data["last_played"]
+            last_played_formatted = time.strftime("%H:%M:%S - %d/%m/%Y", time.localtime(last_played))
+            time_difference = datetime.timedelta(seconds=int(time.time() - last_played))
+            time_difference_formatted = ""
+            if time_difference.days > 0:
+                time_difference_formatted += f"\033[1;36m{time_difference.days}\033[0m \033[3;36mdays, "
+            if time_difference.seconds >= 3600:
+                hours = time_difference.seconds // 3600
+                time_difference_formatted += f"\033[1;36m{hours}\033[0m \033[3;36mhours, "
+            if time_difference.seconds >= 60:
+                minutes = (time_difference.seconds % 3600) // 60
+                time_difference_formatted += f"\033[1;36m{minutes}\033[0m \033[3;36mminutes, "
+            seconds = time_difference.seconds % 60
+            time_difference_formatted += f"\033[1;36m{seconds}\033[0m \033[3;36mseconds ago"
+        print("\033[1;34m     " + game_name + "\033[0m \033[1;32m-\033[0m " + f"{time_difference_formatted}" + "\033[0m") # print in blue
+    game_choice = input("\n \033[7;32m Game choice: \033[0m ")
+    if game_choice == "back to menu":
+        return
+    if game_choice in games:
+        game = games[game_choice]
+        #print(f"Time current: ",int(time.time()))
+        #print(f"Last played: ",int(last_played_old))
+        #print(f"Diference: ",int(time.time() - last_played_old))
+        global random_number
+
+        def generate_number():
+            diff = int(time.time() - last_played_old)
+            global minLimit
+            global maxLimit
+            minLimit = random.randint(10000 + diff, 100000 + diff)
+            maxLimit = random.randint(100000 + diff, 1000000 + diff)
+            random_number = random.randint(minLimit, maxLimit)
+            return random_number
+        random_number = int(generate_number())
+        while True:
+            # os.system('cls') # clear console
+            print("\033[1;32m Guess the number between \033[1;34m{0}\033[0m \033[1;32mand \033[1;34m{1}\033[0m\033[1;32m: \033[0m".format(minLimit,maxLimit))
+            guess = input("\n\033[7;32m Choice: \033[0m ")
+            if guess.upper() == "QUIT":
+                break
+            elif int(guess) < minLimit:
+                print("\033[7;31mInvalid input;\033[0m \033[1;32mPlease enter a number \033[1;34mgreater\033[0m\033[1;32m than \033[1;34m{0}\033[0m\033[1;32m;\033[0m".format(minLimit))
+                countdownMiniGame(2)
+            elif int(guess) > maxLimit:
+                print("\033[7;31mInvalid input;\033[0m \033[1;32mPlease enter a number \033[1;34mless\033[0m\033[1;32m than \033[1;34m{0}\033[0m\033[1;32m;\033[0m".format(maxLimit))
+                countdownMiniGame(2)
+            elif not guess.isdigit():
+                print("\033[7;31mInvalid input;\033[0m \033[1;32mPlease enter a number.\033[0m")
+                countdownMiniGame(2)
+            elif int(guess) > random_number:
+                print("\033[1;32mYou're number is \033[0m\033[1;34mhigher\033[0m\033[1;32m.")
+                countdownMiniGame(2)
+            elif int(guess) < random_number:
+                print("\033[1;32mYou're number is \033[0m\033[1;34mlower\033[0m\033[1;32m.")
+                countdownMiniGame(2)
+            else:
+                print("\033[7;32mCongratulations!\033[0m\033[1;34m You guessed the number!\033[0m")
+                print("\n\033[1;32mLaunching \033[0m\033[1;34m"+game_choice+"\033[0m\033[1;32m in\033[1;32m")
+                countdown(10)
+                game["last_played"] = time.time()
+                with open("platforms.json", "w") as f:
+                    json.dump(platforms, f, indent=4)
+                os.startfile(game["game_location"])
+                break
+
+    else:
+        print("Invalid choice")
+
+def countdown(seconds):
+    for i in range(seconds, 0, -1):
+        print(i)
+        time.sleep(1)
+    print("\033[1;32mStarting game...\033[1;32m")
+    time.sleep(2)
+
+def countdownMiniGame(seconds):
+    print("\nYou can try again in ")
+    for i in range(seconds, 0, -1):
+        print(i)
+        time.sleep(1)
+    print("Restarting...")
+    time.sleep(1)
+    os.system('cls')
+
+with open("platforms.json") as f:
+    platforms = json.load(f)
+
+while True:
+    main_menu()
+    choice = input("\033[7;32m Choice: \033[0m ")
+    os.system('cls' if os.name=='nt' else 'clear')  # Clear console after each choice
+    if choice == "exit":
+        break
+    elif choice == "platforms":
+        platforms_menu(platforms)
+    else:
+        print("Invalid choice")
